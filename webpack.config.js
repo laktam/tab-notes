ca5a8f226900c14c6ecb2b3ac7050ce0c66ae63c
laktam abdellah <laktamlaktamabdellah@gmail.com>
2024-06-15 01:31:46 +0100

    initial project config

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+  entry: {
+    background: './src/background.js',
+    content: './src/content.js',
+    popup: './src/popup.js'
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'build'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  }
+};
