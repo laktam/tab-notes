a782f165f2f620e45b0959b53a1216351c7872ae
laktam <laktamlaktamabdellah@gmail.com>
2024-09-07 15:54:56 +0100

    add notes sidebar and note search on selected page to notesPage.html

--- build/notesPage/notesPage.js
+++ build/notesPage/notesPage.js
@@ -1,16 +1,62 @@
 main();
-
+var selectedNotesKey = ""
+const globalNotesList = []
 function main() {
-
+    const sidebar = document.getElementById("sidebar");
+    const result = document.getElementById("result");
+    
+    chrome.storage.local.get("notes").then((result) => {
+        console.log(`note on local storage : `,result);
+        const notes = result["notes"];
+        for (const notesKey in notes) {
+            const notesList = notes[notesKey];
+            const pageKeyDiv = document.createElement('div');
+            pageKeyDiv.className = "pageKeyDiv"
+            pageKeyDiv.innerText = notesKey
+            console.log("notes key", notesKey)
+            pageKeyDiv.addEventListener('click',
+                ()=>{
+                    // result.innerHTML =
+                    selectedNotesKey = notesKey
+                    searchNotes("", notesKey) 
+                }
+            )
+            sidebar.append(pageKeyDiv)
+        }
+    })
 }
 
 document.getElementById('search').addEventListener('keyup', () => {
     const keyword = document.getElementById('search').value;
-    const notesKey = getNoteKeyFromURL();
-    searchNotes(keyword, notesKey);
+    if(selectedNotesKey == ""){
+        selectedNotesKey = getNoteKeyFromURL();
+    }
+    searchNotes(keyword, selectedNotesKey);
 })
 
 function searchNotes(keyword, notesKey) {
+    const resultDiv = document.getElementById('result');
+    resultDiv.innerText = "";
+    chrome.storage.local.get("notes").then((result) => {
+        const notes = result["notes"] || [];
+        const localNotesList = notes[notesKey];
+
+        for (let note of localNotesList) {
+            const noteString = note.title + note.content + note.link;
+            if (noteString.toLowerCase().includes(keyword.toLowerCase())) {
+                const noteDiv = document.createElement('div');
+                noteDiv.innerText = `${note.title}
+                    ${note.link}
+                    ${note.content}
+                    `
+                resultDiv.appendChild(noteDiv);
+            }
+        }
+
+    });
+}
+
+function searchNotesGlobaly(keyword, notesKey) {
     const resultDiv = document.getElementById('result');
     resultDiv.innerText = "";
     chrome.storage.local.get("notes").then((result) => {
